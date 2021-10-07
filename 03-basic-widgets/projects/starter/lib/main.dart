50856bf7fae8bc6e29fe456589200489b71e1fc4
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-10-07 19:14:20 +0800

    3.4 App structure and navigation

--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'fooderlich_theme.dart';
+import 'home.dart';
 
 void main() {
-  // 1
   runApp(const Fooderlich());
 }
 
@@ -13,27 +13,11 @@ class Fooderlich extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = FooderlichTheme.dark();
-    // TODO: Apply Home widget
-    // 3
     return MaterialApp(
       title: 'Fooderlich',
       theme: theme,
       // 4
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Fooderlich',
-            style: theme.textTheme.headline6,
-          ),
-        ),
-        // TODO: Style the body text
-        body: Center(
-          child: Text(
-            'Let\'s get cooking 👩‍🍳',
-            style: theme.textTheme.headline1,
-          ),
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
