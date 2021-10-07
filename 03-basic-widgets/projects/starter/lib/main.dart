61f5e3a42746c483216aaee2ebcf5d8ef9422ee7
Ryan Yip <71072280+LuckUVeryX@users.noreply.github.com>
2021-10-07 19:35:30 +0800

    Chpt/3 (#1)
    * 3.3 setting a theme
    
    * 3.4 App structure and navigation
    
    * 3.5 Creating custom recipe cards

--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
 void main() {
-  // 1
   runApp(const Fooderlich());
 }
 
@@ -10,19 +12,12 @@ class Fooderlich extends StatelessWidget {
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
-    // TODO: Apply Home widget
-    // 3
+    final theme = FooderlichTheme.dark();
     return MaterialApp(
-      // TODO: Add theme
       title: 'Fooderlich',
+      theme: theme,
       // 4
-      home: Scaffold(
-        // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
-        // TODO: Style the body text
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
-      ),
+      home: const Home(),
     );
   }
 }
