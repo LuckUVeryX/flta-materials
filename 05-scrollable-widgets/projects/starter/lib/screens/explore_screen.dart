cc705105650415793f4640b78666c0d1a3a89aa3
Ryan Yip <71072280+LuckUVeryX@users.noreply.github.com>
2021-10-16 13:55:36 +0800

    Chpt/5 (#2)
    * init ios
    
    * added nested listview
    
    * added gridview
    
    * completed challenge 1
    
    * completed challenge 2

---
+++ 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import '../api/mock_fooderlich_service.dart';
+import '../components/components.dart';
+import '../models/models.dart';
+
+class ExploreScreen extends StatefulWidget {
+  // 1
+
+  const ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  final _controller = ScrollController();
+
+  void _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      print('i am at the bottom');
+    }
+    if (_controller.offset <= _controller.position.minScrollExtent &&
+        !_controller.position.outOfRange) {
+      print('i am at the top');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_scrollListener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return FutureBuilder(
+      // 2
+      future: mockService.getExploreData(),
+      // 3
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        // 4
+        if (snapshot.connectionState == ConnectionState.done) {
+          // 5
+          return ListView(
+            // 6
+            scrollDirection: Axis.vertical,
+            controller: _controller,
+            children: [
+              // 7
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              // 8
+              const SizedBox(height: 16),
+              // 9
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
+            ],
+          );
+        } else {
+          // 10
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
